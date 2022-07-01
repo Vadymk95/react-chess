53f5d5d90b350ebee92f1c311d49895829253ff8
Vadym Kononenko <vadymk95@gmail.com>
2022-07-01 17:14:28 +0300

    chess_logic/feat/figures/added logic

--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -10,4 +10,28 @@ export class King extends Figure {
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.KING;
   }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) {
+      return false;
+    }
+    const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1;
+
+    if (
+      ((target.y === this.cell.y + 1) || (target.y === this.cell.y - 1)) &&
+      ((target.x === this.cell.x + 1) || (target.x === this.cell.x - 1)) &&
+      this.cell.board.getCell(target.x, target.y).isEmpty()
+    ) {
+      return true;
+    }
+
+    if (
+      target.y === this.cell.y + direction &&
+      (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) &&
+      this.cell.isEnemy(target)
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
